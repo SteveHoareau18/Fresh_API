88ae53831d23b1506368258220e82c114e8f6e93
Steve <steve.hoareau1@gmail.com>
2024-03-04 13:44:14 +0400

    init

---
+++ src/test/java/fr/steve/fresh_api/FreshApiApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.steve.fresh_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FreshApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
