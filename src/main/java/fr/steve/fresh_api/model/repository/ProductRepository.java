229991ba89c6a0c885e864610c6149e02eeea33f
Maxime <maximeclaind@gmail.com>
2024-03-05 12:41:53 +0400

    entity,exception  et controller product

---
+++ src/main/java/fr/steve/fresh_api/model/repository/ProductRepository.java
@@ -0,0 +1,11 @@
+package fr.steve.fresh_api.model.repository;
+
+import java.util.List;
+
+import org.springframework.data.repository.CrudRepository;
+
+import fr.steve.fresh_api.model.entity.Product;
+
+public interface ProductRepository extends CrudRepository<Product, Integer> {
+
+    List<Product> findByName(String Name);}
