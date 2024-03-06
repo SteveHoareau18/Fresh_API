76fd47c8c856cd45c412928b1111fd68c1d855b8
Steve <steve.hoareau1@gmail.com>
2024-03-06 16:20:57 +0400

    websocket allow origin example

--- src/main/java/fr/steve/fresh_api/config/SecurityConfig.java
+++ src/main/java/fr/steve/fresh_api/config/SecurityConfig.java
@@ -1,5 +1,7 @@
 package fr.steve.fresh_api.config;
 
+import java.util.Arrays;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
@@ -17,6 +19,12 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.logout.HttpStatusReturningLogoutSuccessHandler;
 import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
+<<<<<<< HEAD
+=======
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+>>>>>>> 642c3b9 (websocket allow origin example)
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
