d5bdf688bd922174ff8cb08881c7d536e72845f1
Steve <steve.hoareau1@gmail.com>
2024-03-07 08:49:48 +0400

    app prop

--- src/main/java/fr/steve/fresh_api/config/SecurityConfig.java
+++ src/main/java/fr/steve/fresh_api/config/SecurityConfig.java
@@ -1,7 +1,5 @@
 package fr.steve.fresh_api.config;
 
-import java.util.Arrays;
-
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
@@ -20,11 +18,14 @@ import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.logout.HttpStatusReturningLogoutSuccessHandler;
 import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 <<<<<<< HEAD
+<<<<<<< HEAD
 =======
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 >>>>>>> 642c3b9 (websocket allow origin example)
+=======
+>>>>>>> e9c0424 (app prop)
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
