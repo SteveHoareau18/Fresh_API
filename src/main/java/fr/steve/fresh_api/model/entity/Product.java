44b77562f63bcc031de77cf1cf5cdfe7eff9c535
Steve Hoareau <76117439+SteveHoareau18@users.noreply.github.com>
2024-03-07 16:00:41 +0400

    refactor (#32)

--- src/main/java/fr/steve/fresh_api/model/entity/Product.java
+++ src/main/java/fr/steve/fresh_api/model/entity/Product.java
@@ -1,10 +1,6 @@
 package fr.steve.fresh_api.model.entity;
 
-import org.springframework.data.annotation.CreatedBy;
-import org.springframework.data.jpa.domain.support.AuditingEntityListener;
-
 import jakarta.persistence.Entity;
-import jakarta.persistence.EntityListeners;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
