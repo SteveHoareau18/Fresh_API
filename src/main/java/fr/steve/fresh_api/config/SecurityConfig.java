d3de6d3bed29539b594672ee293d60dff4935c4f
Steve <steve.hoareau1@gmail.com>
2024-03-07 10:23:41 +0400

    rebase main feature/web-socket

--- src/main/java/fr/steve/fresh_api/config/SecurityConfig.java
+++ src/main/java/fr/steve/fresh_api/config/SecurityConfig.java
@@ -1,5 +1,7 @@
 package fr.steve.fresh_api.config;
 
+import java.util.Arrays;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
