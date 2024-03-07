5c470d494ac7e3a213bcdd142c6220812bbe365c
Steve <steve.hoareau1@gmail.com>
2024-03-07 09:35:30 +0400

    app prop

--- src/main/java/fr/steve/fresh_api/FreshApiApplication.java
+++ src/main/java/fr/steve/fresh_api/FreshApiApplication.java
@@ -2,10 +2,13 @@ package fr.steve.fresh_api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 @EnableJpaAuditing
+@PropertySource(value = {"classpath:application-local.properties"},
+                ignoreResourceNotFound = true)
 public class FreshApiApplication {
 
 	public static void main(String[] args) {
