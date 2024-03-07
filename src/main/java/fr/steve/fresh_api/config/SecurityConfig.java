3c437870f5b75b9ea693a1dd17eace8a87471d7b
Steve <steve.hoareau1@gmail.com>
2024-03-07 08:49:48 +0400

    app prop

--- src/main/java/fr/steve/fresh_api/config/SecurityConfig.java
+++ src/main/java/fr/steve/fresh_api/config/SecurityConfig.java
@@ -1,7 +1,5 @@
 package fr.steve.fresh_api.config;
 
-import java.util.Arrays;
-
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
