6d3ae14d2303f361ba3e3a009e370fc052a693be
Steve <steve.hoareau1@gmail.com>
2024-03-08 15:30:10 +0400

    fix merge

--- src/main/java/fr/steve/fresh_api/model/dto/course_product/UpdateCourseProductDto.java
+++ src/main/java/fr/steve/fresh_api/model/dto/course_product/UpdateCourseProductDto.java
@@ -3,7 +3,6 @@ package fr.steve.fresh_api.model.dto.course_product;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 import fr.steve.fresh_api.model.dto.product.CreateProductDto;
-
 import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -20,6 +19,5 @@ public class UpdateCourseProductDto {
     private String commentary;
     @JsonProperty("product_id")
     private Integer productId;
-    private Long userId;
     private CreateProductDto product;
 }
